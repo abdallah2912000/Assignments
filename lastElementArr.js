4c374fa84bfcccaf9482efe3a0a552b30deed28f
Abdullah Khaled Badr <107265014+abdallah2912000@users.noreply.github.com>
2024-03-04 17:42:45 +0200

    Update lastElementArr.js

--- lastElementArr.js
+++ lastElementArr.js
@@ -2,7 +2,7 @@ let arr1 = [1, 2, null, undefined, "abdo"];
 
 Array.prototype.last = function () {
   if (this.length >= 0 && this.length <= 1000) {
-    if (this.length === 0) return -1;
+    if (this.length === 0) return undefined;
     return this[this.length - 1];
   } else {
     throw new SyntaxError("Array length should 0 <= Array.length <= 1000");
